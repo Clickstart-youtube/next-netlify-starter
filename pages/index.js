1cccf737e4e550aeeb1f79988b71bbfc7b89bf69
Clickstart-youtube <98811026+Clickstart-youtube@users.noreply.github.com>
2024-05-15 17:18:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,25 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+function previewImage(event) {
+  var reader = new FileReader();
+  reader.onload = function() {
+    var preview = document.getElementById('preview-image');
+    preview.src = reader.result;
+  }
+  reader.readAsDataURL(event.target.files[0]);
+}
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function uploadImage() {
+  var fileInput = document.getElementById('file-input');
+  var imageGallery = document.getElementById('image-gallery');
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
+  if (fileInput.files.length > 0) {
+    var file = fileInput.files[0];
+    var imageUrl = URL.createObjectURL(file);
+    var imageElement = document.createElement('img');
+    imageElement.src = imageUrl;
+    imageElement.alt = 'Homework';
+    imageGallery.appendChild(imageElement);
+    fileInput.value = ''; // Clear the file input after upload
+  } else {
+    alert('Please select a file to upload.');
+  }
 }
